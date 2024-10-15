03ce230dab3b66edd9c88e2c782d33e26041ab27
Michal Skrzypczynski <276157@student.pwr.edu.pl>
2024-10-15 22:44:54 +0200

    Calculator.java added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static double addition(double a, double b){
+        return a + b;
+    }
+
+    public static double subtraction(double a, double b){
+        return a - b;
+    }
+}
